af28932b1db2b781b4a2b04e28f111be1e8d2fc1
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-27 15:44:41 +0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t telegram_bot  .
+docker stop telegram_bot || true && docker rm telegram_bot || true
+docker run -d -p 3000:3000 --name telegram_bot --restart unless-stopped telegram_bot
